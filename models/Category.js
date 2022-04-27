2c7543eb2e00efa2292a2223fc99b3ad5e4cff58
M8MBA <sauce.ix@gmail.com>
2022-04-27 16:29:40 -0600

    Define columns for the model

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,17 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define category_name column
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
